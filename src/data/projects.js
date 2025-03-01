a5f375888683093976b8a34bc4c959a1c8e64ec3
mlz <mlz@mit.edu>
2025-02-28 22:51:28 -0500

    gif

--- src/data/projects.js
+++ src/data/projects.js
@@ -1,7 +1,7 @@
 const data = [
   {
-    title: 'Paper Published',
-    image: '/images/projects/TBD.png',
+    title: 'Paper Published in Nature Science and Cell, no, jk',
+    image: '/images/projects/4D-STEM.gif',
     link: '',
     date: '2021-05-01',
     desc:
