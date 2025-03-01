4d6446e6036033f966d0294f2ce1f50a8ee9c80b
mlz <mlz@mit.edu>
2025-03-01 17:16:27 -0500

    more projects

--- src/data/projects.js
+++ src/data/projects.js
@@ -1,12 +1,5 @@
 const data = [
   {
-    title: 'Operando Obervation of AFE-to-FE Transtion',
-    image: '/images/projects/PZO.gif',
-    link: 'https://www.nature.com/articles/s41563-024-01907-y',
-    date: '2024-05-23',
-    desc: 'Operando electron microscopy reveals the field-induced antiferroelectric-to-ferroelectric phase transition in thin-film PbZrO₃, marked by oxygen octahedra detilting and lattice-volume expansion. This transition, influenced by substrate constraints, results in enhanced electromechanical strain (1.7%) in 100 nm thick films. The atomic-scale pathway, including an intermediate transient phase, is also revealed. First-principles calculations support the mechanism, offering insights into thickness scaling for high-performance micro-/nano-electromechanical systems.',
-  },
-  {
     title: 'Polar Slush of Relaxor Ferroelectrics in 3D',
     image: '/images/projects/PMN.png',
     link: 'https://arxiv.org/abs/2408.11685',
@@ -15,12 +8,43 @@ const data = [
       'Disruption of long-range order through heterogeneity can drastically alter material behavior, as seen in lead-based relaxor ferroelectrics. Decades of research have linked local chemical and structural inhomogeneities to unique macroscopic properties like high electromechanical coupling and broad dielectric response. While it is generally accepted that these inhomogeneities promote the relaxor response, various interpretations have emerged, ranging from the early model of polar nanoregions to the more recent polar nanodomain slush model. Here, we directly oberve the ploar slush in 3D using mutlislice electron ptychograpy.',
   },
   {
+    title: 'Operando Obervation of AFE-to-FE Transtion',
+    image: '/images/projects/PZO.gif',
+    link: 'https://www.nature.com/articles/s41563-024-01907-y',
+    date: '2024-05-23',
+    desc: 'Operando electron microscopy reveals the field-induced antiferroelectric-to-ferroelectric phase transition in thin-film PbZrO₃, marked by oxygen octahedra detilting and lattice-volume expansion. This transition, influenced by substrate constraints, results in enhanced electromechanical strain (1.7%) in 100 nm thick films. The atomic-scale pathway, including an intermediate transient phase, is also revealed. First-principles calculations support the mechanism, offering insights into thickness scaling for high-performance micro-/nano-electromechanical systems.',
+  },
+  {
     title: '3D View of APBs and Spatially Corrleated AFE Nanodomains',
     image: '/images/projects/PMW.png',
     link: 'https://pubs.acs.org/doi/abs/10.1021/acsnano.4c14833',
     date: '2025-01-27',
     desc:
       'Multislice electron ptychography is used to characterize a chemical antiphase boundary in the double perovskite Pb₂MgWO₆, revealing its inclined orientation and finite chemical intermixing. 3D imaging shows antiferroelectric-like displacements near the boundary, contrasting with the paraelectric bulk. Spatial statistics and DFT calculations suggest these boundaries form due to kinetic constraints, with local chemistry influencing polar distortions. This study highlights the interplay between chemistry and polarization at APBs, offering insights for ferroic thin films.',
+  },
+  {
+    title: 'Emergent Interfacial Magnetic States',
+    image: '/images/projects/LFO.png',
+    link: 'https://arxiv.org/pdf/2405.12950',
+    date: '2024-5-21',
+    desc:
+      'Atomic resolution imaging combined with monochromated EELS reveals interfacial-mediated ferromagnetism driven by spin disproportionation in LaFeO₃/SrTiO₃. Epitaxial strain induces a checkerboard arrangement of low- and high-spin Fe<sup>3+</sup> ions at the interface, leading to ferromagnetism via superexchange interactions, offering insights for designing spintronic materials.',
+  },
+  {
+    title: 'Machine Learning Assisted Phase Mapping from 4D-STEM',
+    image: '/images/projects/ML.png',
+    link: 'https://www.nature.com/articles/s41598-024-54661-1',
+    date: '2024-02-20',
+    desc:
+      'High-resolution characterization of domain structures across large scales is essential for unlocking the potential of domains and domain walls. 4D-STEM facilitates the collection of rich, spatially resolved diffraction data, yet traditional analysis methods often demand extensive prior structural knowledge. Integrating 4D-STEM with machine learning—both supervised and unsupervised—provides a powerful, scalable approach to analyze massive datasets and uncover hidden patterns without relying on preconceived models, opening new avenues for advanced material design and functional optimization.',
+  },
+  {
+    title: 'Atomic Scale Debye Waller Thermometry',
+    image: '/images/projects/DWT.png',
+    link: 'https://www.sciencedirect.com/science/article/abs/pii/S0304399121001972',
+    date: '2022-01-01',
+    desc:
+      'Nanoscale temperature measurements are crucial for understanding the local thermal properties. As atoms vibrate more intensely with increasing temperature due to the larger Debye-Waller factor, the electron scattering cross sections are affected, leading to changes in the contrast of STEM images. This temperature sensitivity, experimentally demonstrated using 4D-STEM as a function of scattering angle, align well with multislice simulations, offering a promising method for  atomic-scale temperature measurements.',
   },
 ];
 export default data;
