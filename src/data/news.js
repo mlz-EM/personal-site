c44a69df32e0f98de3092e494835c29427368dd7
mlz <mlz@mit.edu>
2026-06-16 09:07:57 -0400

    misc

---
+++ src/data/news.js
@@ -0,0 +1,123 @@
+// Optional per-item UI flags:
+// - set `expandable: true` to enable detail expansion
+// - set `pinned: true` to pin item to top + home featured section
+const data = [
+  {
+    label: 'Invited Talk: International Cryo-EM (ICE) Workshop',
+    link: 'https://www.bnl.gov/iceworkshop/files/pdf/agenda.pdf',
+    image: '/images/misc/BNL.png',
+    date: '2026-06-06',
+    category: 'Misc',
+    expandable: false,
+    pinned: true,
+    desc:
+    '',
+  },
+  {
+    label: 'Paper Published: Relaxor Seen in 3D by MEP',
+    link: 'https://www.science.org/doi/10.1126/science.ads6023',
+    image: '/images/misc/science.png',
+    date: '2026-04-29',
+    category: 'Paper',
+    expandable: false,
+    pinned: true,
+    desc:
+    '',
+  },
+  {
+    label: 'DMSE News on Our Relaxor Paper',
+    link: 'https://dmse.mit.edu/news/the-hidden-structure-behind-a-widely-used-class-of-materials/',
+    image: '/images/misc/DMSE.png',
+    date: '2026-04-29',
+    category: 'Misc',
+    expandable: false,
+    pinned: false,
+    desc:
+    '',
+  },
+  {
+    label: 'MIT Press on Our Relaxor Paper',
+    link: 'https://news.mit.edu/2026/hidden-structure-behind-widely-used-class-materials-0430',
+    image: '/images/misc/MIT.svg',
+    date: '2026-04-29',
+    category: 'Misc',
+    expandable: false,
+    pinned: false,
+    desc:
+    '',
+  },
+  {
+    label: 'MSA Albert Crewe Award 🎉 🙏',
+    link: 'https://microscopy.org/msa-society-awards-recipients#albert:~:text=2026-,Menglin%20Zhu,-401%20Edgewater%20Place',
+    image: '/images/misc/MSA.png',
+    date: '2026-03-24',
+    category: 'Misc',
+    expandable: false,
+    pinned: true,
+    desc:
+    '',
+  },
+  {
+    label: 'Paper Published: Operando Biasing on Non-Conductive Substrates',
+    link: 'https://academic.oup.com/mam/article/32/2/ozag007/8504233',
+    image: '/images/misc/mm.svg',
+    date: '2026-03-03',
+    category: 'Paper',
+    expandable: false,
+    desc:
+    '',
+  },
+  {
+    label: 'Paper Published: Phaser (one of the best ptychography softwares)',
+    link: 'https://www.nature.com/articles/s41524-026-01956-8',
+    image: '/images/misc/npj.png',
+    date: '2026-01-18',
+    category: 'Paper',
+    expandable: false,
+    desc:
+    '',
+  },
+  {
+    label: 'ArXiv: Field-Driven Phase Transformation in PbZrO3 at Atomic Scale',
+    link: 'https://arxiv.org/abs/2509.07194',
+    image: '/images/misc/arxiv.jpg',
+    date: '2025-08-01',
+    category: 'Paper',
+    expandable: false,
+    desc:
+    '',
+  },
+  {
+    label: 'ArXiv: Operando S/TEM Biasing on Non-Conductive Substrates',
+    link: 'https://arxiv.org/abs/2508.12503',
+    image: '/images/misc/arxiv.jpg',
+    date: '2025-08-17',
+    category: 'Paper',
+    expandable: false,
+    desc:
+    '',
+  },
+  {
+    label: 'Paper Published: Interfacial FM Induced by Spin State Transition',
+    link: 'https://advanced.onlinelibrary.wiley.com/doi/10.1002/admi.202500169',
+    image: '/images/misc/advanced_materials_interfaces.jpg',
+    date: '2025-05-22',
+    category: 'Paper',
+    expandable: false,
+    desc:
+    '',
+  },
+  {
+    label: 'Personal Page Deployed',
+    link: 'https://mlz-em.github.io/personal-site/',
+    image: '/images/misc/personal-page-deployed.png',
+    date: '2025-02-28',
+    category: 'Misc',
+    expandable: false,
+    pinned: false,
+    desc:
+    '',
+  },
+];
+
+export default data;
